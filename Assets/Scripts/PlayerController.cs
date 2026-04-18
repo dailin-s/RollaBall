8ec614d853fbef4a8b41613a432dc6ccc0b3fc4a
dailin <dailinsiggers@gmail.com>
2026-04-17 21:47:53 -0500

    player movement
    added player movement, working on player speed and scene building next

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    private Vector2 movementVector;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent <Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void OnMove (InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(movementVector);
+        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+        rb.AddForce(movement);
+        rb.AddForce(movementVector); 
+    }
+
+}
