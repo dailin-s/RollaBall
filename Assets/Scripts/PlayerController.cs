1f84527c6a5762fd71e2011bb36f68793f8b2e6a
dailin <dailinsiggers@gmail.com>
2026-04-22 22:30:02 -0500

    added movement
    added player movement and also added camera movement script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody rb;
     private float movementX;
     private float movementY;
-    private Vector2 movementVector;
+    public float speed = 0; 
+
+   
     
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,16 +24,19 @@ public class Player : MonoBehaviour
     void OnMove (InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
+
         movementX = movementVector.x;
         movementY = movementVector.y;
+
+        Debug.Log("Move triggered");
     }
 
     void FixedUpdate()
     {
-        rb.AddForce(movementVector);
-        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
-        rb.AddForce(movement);
-        rb.AddForce(movementVector); 
+        Vector3 movement = new Vector3 (movementX, 1.0f, movementY);
+
+        rb.AddForce(movement * speed);
+ 
     }
 
 }
