0617e3e4de87391522d1a3d1fe9a921d5c1f5e70
dailin <dailinsiggers@gmail.com>
2026-04-27 23:07:43 -0500

    added major game updates
    added enemy's and gave them ai behavior via enemy movement and navmaps, added obstacles (with its own material), added chips to collect + counter for score and rotation on them. added you win/lose screens. game is complete :D

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
@@ -10,14 +11,18 @@ public class Player : MonoBehaviour
     private float movementX;
     private float movementY;
     public float speed = 0; 
-
-   
-    
+    private int count;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent <Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
+
     }
 
     // Update is called once per frame
@@ -31,6 +36,18 @@ public class Player : MonoBehaviour
         Debug.Log("Move triggered");
     }
 
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if(count >= 8)
+        {
+            winTextObject.SetActive(true);
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+
+        }
+    }
+
     void FixedUpdate()
     {
         Vector3 movement = new Vector3 (movementX, 1.0f, movementY);
@@ -38,5 +55,27 @@ public class Player : MonoBehaviour
         rb.AddForce(movement * speed);
  
     }
+
+    void OnTriggerEnter(Collider other) 
+   {
+       if (other.gameObject.CompareTag("PickUp")) 
+       {
+           other.gameObject.SetActive(false); 
+           count = count + 1;
+           SetCountText();
+       }
+   }
+
+   private void OnCollisionEnter(Collision collision)
+{
+   if (collision.gameObject.CompareTag("Enemy"))
+   {
+       Destroy(gameObject); 
+
+       // Update the winText to display "You Lose!"
+       winTextObject.gameObject.SetActive(true);
+       winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+   }
+}
 
 }
